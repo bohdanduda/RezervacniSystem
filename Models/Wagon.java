02a97e4b85582aea540482bdec3edd80f19d7f81
Bohdan Duda <duda.bohdan23@gmail.com>
2023-11-26 20:14:56 +0100

    Complete wagon class

--- Models/Wagon.java
+++ Models/Wagon.java
@@ -1,4 +1,31 @@
 package Models;
 
 public class Wagon {
+    private int id;
+    private boolean[][] seats;
+
+    public Wagon(int id, int seatRowCount) {
+        this.id = id;
+        this.seats = new boolean[2][seatRowCount];
+    }
+
+    public void reserveSeat(int seat, int row) {
+        if (!seats[seat][row]) {
+            seats[seat][row] = true;
+
+            System.out.println("Your seat has been reserved!");
+        }
+
+        System.out.println("This seat is already occupied!");
+    }
+
+    public void vacateSeat(int seat, int row) {
+        if (seats[seat][row]) {
+            seats[seat][row] = false;
+
+            System.out.println("Your seat has been vacated!");
+        }
+
+        System.out.println("This seat is empty!");
+    }
 }
